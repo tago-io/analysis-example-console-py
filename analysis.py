006afc2047858de8c6c3fbd4d0aacefc0bf6c4e3
Mateus Cardoso <mateus.informatica44@gmail.com>
2023-03-01 14:31:09 -0300

    Update to use new version of sdk-python.

--- analysis.py
+++ analysis.py
@@ -1,21 +1,22 @@
-# Analysis Example
-# Hello World
+"""
+Analysis Example
+Hello World
 
-# Learn how to send messages to the console located on the TagoIO analysis screen.
-# You can use this principle to show any information during and after development.
-
-from tago import Analysis
+Learn how to send messages to the console located on the TagoIO analysis screen.
+You can use this principle to show any information during and after development.
+"""
+from tagoio_sdk import Analysis
 
 # The function myAnalysis will run when you execute your analysis
-def myAnalysis(context, scope):
+def myAnalysis(context, scope: list) -> None:
   # This will log "Hello World" at the TagoIO Analysis console
-  context.log("Hello World")
+  print("Hello World")
 
   #  This will log the environment to the TagoIO Analysis console
-  context.log('Environment:', context.environment)
+  print("Environment:", context.environment)
 
   #  This will log the scope to the TagoIO Analysis console
-  context.log('my scope:', scope)
+  print("my scope:", scope)
 
 # The analysis token in only necessary to run the analysis outside TagoIO
-Analysis('MY-ANALYSIS-TOKEN-HERE').init(myAnalysis)
+Analysis({"token": "MY-ANALYSIS-TOKEN-HERE"}).init(myAnalysis)
